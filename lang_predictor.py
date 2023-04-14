980178864965a56ca267106c537c99b59719c9ff
Kimaya Ved <102381197+kimayav@users.noreply.github.com>
2023-04-15 02:16:34 +0530

    updated predictor

--- lang_predictor.py
+++ lang_predictor.py
@@ -9,24 +9,22 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import re
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-data = pd.read_csv('/language_dataset.csv')
-X = data["text"]
-y = data["language"]
-le = LabelEncoder()
+data = pd.read_csv('/languageDataset.csv')
+X = data["Text"]
+y = data["Language"]
+le = LabelEncoder() 
 y = le.fit_transform(y)
 data_list = []
-# iterating through all the text
 for text in X:
-       # removing the symbols and numbers
+
         text = re.sub(r'[!@#$(),n"%^*?:;~`0-9]', ' ', text)
         text = re.sub(r'[[]]', ' ', text)
-        # converting the text to lower case
         text = text.lower()
-        # appending to data_list
+
         data_list.append(text)
 cv = CountVectorizer()
 X = cv.fit_transform(data_list).toarray()
-X.shape # (10337, 39419)
+X.shape
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 from sklearn.naive_bayes import MultinomialNB
@@ -36,15 +34,11 @@ y_pred = model.predict(x_test)
 ac = accuracy_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred)
 
-print("Accuracy is :",ac*100,'%')
-# plt.figure(figsize=(15,10))
-# sns.heatmap(cm, annot = True)
-# plt.show()
-
 def predict(text):
     x = cv.transform([text]).toarray()
     lang = model.predict(x)
     lang = le.inverse_transform(lang)
-    print("The langauge is in",lang[0])
+    return lang[0]
+inp=input("ENTER THE SENTENCE:")
+predict(inp)
 
-predict("Excusez")
